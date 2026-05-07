18eb8463e50fa977648aef63def9cd0b2781d072
Erik Camacho <flasher29@gmail.com>
2026-05-07 14:49:56 -0600

    Agregar modulos SG y ec2 a ambientes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,42 +7,21 @@ module "vpc" {
   public_subnet_name = "${var.project_name}-public-subnet"
 }
 
-resource "aws_security_group" "web_sg" {
-  name   = "${var.project_name}-sg"
+module "sg" {
+  source = "../../modulos/security-group"
+  name = "${var.project_name}-sg"
   vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  port = 80
+  cidr = ["0.0.0.0/0"]
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-014d82945a82dfba3"
+module "deploy_instance" {
+  source = "../../modulos/ec2"
+  ami = "ami-014d82945a82dfba3"
   instance_type = "t3.micro"
   subnet_id     = module.vpc.public_id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "${var.project_name}-ec2"
-  }
+  vpc_sg_ids = [module.sg.sg_id]
+  name = "${var.project_name}-ec2"
 }
 
 module "deploy_bucket" {
