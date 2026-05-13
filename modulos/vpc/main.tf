7a419c81f5c897182318c13e2a4501f47166a9c3
Erik Camacho <flasher29@gmail.com>
2026-05-13 12:44:26 -0600

    Agregar nuevos modulos

--- modulos/vpc/main.tf
+++ modulos/vpc/main.tf
@@ -15,3 +15,40 @@ resource "aws_subnet" "public" {
     Name = var.public_subnet_name
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.private_subnet
+  tags = {
+    "Name" = var.private_subnet_name
+  }
+  depends_on = [
+    aws_subnet.public
+  ]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+resource "aws_route_table" "public_crt" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = var.cidr_route_table
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.public_crt_name
+  }
+}
+
+resource "aws_route_table_association" "crta_public_subnet" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_crt.id
+}
--- modulos/vpc/variables.tf
+++ modulos/vpc/variables.tf
@@ -2,3 +2,11 @@ variable "cidr_block" {}
 variable "name" {}
 variable "public_subnet" {}
 variable "public_subnet_name" {}
+variable "private_subnet" {
+  description = "CIDR private subnet"
+  type = string
+}
+variable "private_subnet_name" {}
+variable "igw_name" {}
+variable "cidr_route_table" {}
+variable "public_crt_name" {}
